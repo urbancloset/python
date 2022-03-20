60f60fe1620875bf67d871b30fe4ded41c7c240b
urbancloset <urbancloset4321@gmail.com>
2022-03-20 23:19:21 +0530

    Updated

--- NON GUI/delete.py
+++ NON GUI/delete.py
@@ -1,9 +1,14 @@
 import mysql.connector
 import mysql
-sid = input("Enter Id : ")
 
 conn = mysql.connector.connect(host="localhost", user="root", password="", database="test")
 cursor = conn.cursor()
+selectStudent = "select * from student"
+
+cursor.execute(selectStudent)
+result = cursor.fetchall()
+print(result)
+sid = input("Enter Id : ")
 deleteStudent = "delete from student where Id = '"+sid+"'"
 
 cursor.execute(deleteStudent)
--- NON GUI/update.py
+++ NON GUI/update.py
@@ -1,12 +1,18 @@
 import mysql.connector
 import mysql
+
+
+conn = mysql.connector.connect(host="localhost", user="root", password="", database="test")
+cursor = conn.cursor()
+selectStudent = "select * from student"
+
+cursor.execute(selectStudent)
+result = cursor.fetchall()
+print(result)
 sid = input("Enter Id : ")
 name = input("Enter Name : ")
 course = input("Enter Course : ")
 semester = input("Enter Semester : ")
-
-conn = mysql.connector.connect(host="localhost", user="root", password="", database="test")
-cursor = conn.cursor()
 updateStudent = "update student set name = '"+name+"', course='"+course+"', semester='"+semester+"' where Id = '"+sid+"'"
 
 cursor.execute(updateStudent)
